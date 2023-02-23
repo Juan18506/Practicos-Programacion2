3ca5bcfaa1fe46097ec05e026a16233ff9a3ae43
Juan18506 <juan185067@gmail.com>
2023-02-23 03:09:56 -0300

    Cambios en la funcion PromClase

--- introducción_C/p0_ej1/p0_ej1.cpp
+++ introducción_C/p0_ej1/p0_ej1.cpp
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
 int PromClase(int cal1, int cal2, int cal3, int cal4, int cal5) {
-  return (cal1 + cal2 + cal3 + cal4 + cal5) / 5;
+  int calificaciones[5] = {cal1, cal2, cal3, cal4, cal5};
+  int menorCalificacion = calificaciones[0];
+
+  for (int i = 1; i < 5; i++) {
+    if (calificaciones[i] < menorCalificacion) {
+      menorCalificacion = calificaciones[i];
+    };
+  };
+
+  return (cal1 + cal2 + cal3 + cal4 + cal5 - menorCalificacion) / 4;
 }
 
-int main(int argc, char const *argv[]) 
+int main(int argc, char const *argv[])
 {
   int cal1, cal2, cal3, cal4, cal5;
   printf("Put the five califications: ");
