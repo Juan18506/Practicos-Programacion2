52974a60a67e130c84dc8982580337675c1bc5d3
Juan18506 <juan185067@gmail.com>
2023-02-26 03:53:11 -0300

    Cambios en la identacion del codigo

--- introducción_C/p0_ej1/p0_ej1.cpp
+++ introducción_C/p0_ej1/p0_ej1.cpp
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-int PromClase(int cal1, int cal2, int cal3, int cal4, int cal5) {
+int PromClase(int cal1, int cal2, int cal3, int cal4, int cal5)
+{
   int calificaciones[5] = {cal1, cal2, cal3, cal4, cal5};
   int menorCalificacion = calificaciones[0];
 
-  for (int i = 1; i < 5; i++) {
-    if (calificaciones[i] < menorCalificacion) {
+  for (int i = 1; i < 5; i++)
+  {
+    if (calificaciones[i] < menorCalificacion)
+    {
       menorCalificacion = calificaciones[i];
     };
   };
