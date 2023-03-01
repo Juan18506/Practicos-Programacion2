91b4939d1a229894b5722cde2ba51ea64e7d46f5
Juan Andres Alves Pedreira Luceno - ENS <juan.andres.alves.pedreira@pcunix45.fing.edu.uy>
2023-03-01 12:19:47 -0300

    Version preliminar de los ej 4,5 del practico 0

---
+++ introduccion_C/p0_ej5/p0_ej5.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct rep_punto {
+    int coordX, coordY;
+} Punto;
+
+typedef struct rep_colPuntos {
+    int capacidad;
+    int cantidad;
+    Punto *arregloPuntos;
+} ColPuntos;
+
+void insertarPunto(ColPuntos &colPtos, Punto pto) {
+
+}
+
+int main() {
+
+}
