4fc2995c67899b99fcd2902167d432c0f1d6fb81
Juan18506 <juan185067@gmail.com>
2023-03-10 00:17:26 -0300

    Cambios en la identacion

--- introduccion_C/p0_ej2/p0_ej2.cpp
+++ introduccion_C/p0_ej2/p0_ej2.cpp
@@ -1,33 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 
-bool esPrimo(int num)
-{
+bool esPrimo(int num) {
   int i = 2;
   int limite = trunc(sqrt(num));
-
-  while ((i <= limite) && (num % i != 0))
+  while ((i <= limite) && (num % i != 0)) {
     i++;
-
+  }
   return !(i <= limite) && (num != 0) && (num != 1);
 }
 
-void PrimosEntreAyB(int a, int b)
-{
-  if (a > b)
-  {
+void PrimosEntreAyB(int a, int b) {
+  if (a > b) {
     int aux = a;
     a = b;
     b = aux;
   };
-
-  for (int i = a; i <= b; i++)
-    if (esPrimo(i))
+  for (int i = a; i <= b; i++) {
+    if (esPrimo(i)) {
       printf("%d \n", i);
+    }
+  }
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
   int a, b;
   printf("Ingresa 2 numeros enteros: ");
   scanf("%d %d", &a, &b);
